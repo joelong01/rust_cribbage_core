7ec98c74a459189e9b74449f594f989d82be3f1d
Rich Czyzewski <richarcz@microsoft.com>
2021-09-15 16:34:30 -0700

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,1 @@
-// This is required for integration tests (in the tests folder) to be able test/utilize
-// the underlying modules. main.rs is inaccessible to tests in the the tests folder.
 pub mod game;
