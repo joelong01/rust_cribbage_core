25d70c43a979c2d0f0f72f1fc3603eb4f698970b
Joe Long <joelong@outlook.com>
2021-09-28 16:02:50 -0700

    fixing up mistaken push

--- api/src/handlers.rs
+++ api/src/handlers.rs
@@ -1,41 +1,5 @@
-use actix_web::{
-    error::ResponseError, get, http::StatusCode, post, web, HttpResponse, Responder, Result,
-};
-use serde::Deserialize;
-use thiserror::Error;
-
-#[derive(Deserialize)]
-struct AiInfo {
-    host_name: String,
-    friendly_name: String,
-}
-
-#[derive(Error, Debug)]
-enum RegistrationError {
-    #[error("Bad config")]
-    BadConfig,
-    #[error("Access to Cosmos failed")]
-    Forbidden,
-}
+use actix_web::Responder;
 
 pub async fn game() -> impl Responder {
-    HttpResponse::Ok().body("Welcome to Cribbage Rust!!!")
-}
-
-#[get("/availableAis")]
-async fn get_available_ais() -> impl Responder {
-    let account_name;
-    match std::env::var("RUST_CRIBBAGE_COSMOS_ACCOUNT_NAME") {
-        Ok(val) => account_name = val,
-        Err(_e) => {}
-    }
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/registerAi/{host_name}/{friendly_name}")]
-async fn register_ai(path: web::Path<AiInfo>) -> Result<String> {
-    Ok(format!(
-        "registered {}/{}",
-        path.host_name, path.friendly_name
-    ))
+    format!("Welcome to Cribbage Rust!!!")
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -4,9 +4,13 @@ use actix_web::{web, App, HttpServer};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+
     // Start http server
-    HttpServer::new(|| App::new().route("v1.0/game", web::get().to(handlers::game)))
-        .bind("127.0.0.1:8080")? // TODO pull address:port from config
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .route("/game", web::get().to(handlers::game))
+    })
+    .bind("127.0.0.1:8080")? // TODO pull address:port from config
+    .run()
+    .await
 }
